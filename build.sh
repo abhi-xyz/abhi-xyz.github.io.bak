1324f96d3a403f5d3f76184dcafc2541f4571915
Abhinandh S <ugabhi@proton.me>
2024-11-28 10:57:58 +0530

    -

--- build.sh
+++ build.sh
@@ -5,10 +5,3 @@ cp -r dist docs
 cp .nojekyll docs/
 cp 404.html docs/
 git add -A && git commit -m "-" && git push
-
-cd dist/
-tar -cvz * > site.tar.gz
-mv site.tar.gz ../site.tar.gz 
-cd ..
-hut pages publish -d abhinandh.srht.site site.tar.gz
-
